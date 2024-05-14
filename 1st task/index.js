32bc56d70cb15d91b66afde47658c49a03246a31
yadavgopinath <yadavgopinath93@gmail.com>
2024-05-14 15:57:01 +0530

    1st task

---
+++ 1st task/index.js
@@ -0,0 +1,13 @@
+// Write your code below:
+const main=document.getElementById('main-heading');
+main.textContent='Fruit World';
+main.style.color='orange';
+const header=document.getElementById('header');
+header.style.backgroundColor='green';
+header.style.borderBottom='3px solid orange';
+
+const basketheading=document.getElementById('basket-heading');
+
+basketheading.style.color='green';
+const thanks=document.getElementById('thanks');
+thanks.innerHTML='<p>Please visit us again</p>';
